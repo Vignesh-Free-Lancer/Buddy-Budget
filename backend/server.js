592b633cc1dae81e54ad000e29d1850eecd6bddd
Vignesh R <developer.vicki@gmail.com>
2022-05-19 20:16:18 +0530

    modified the file for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -26,16 +27,39 @@ app.use("/", groceryRoutes); // Use grocery routes for api call
 app.use("/", reportRoutes); // Use report routes for api call
 app.use("/", feedbackRoutes); // Use feedback routes for api call
 
+/* Deployment  Code Start */
+
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  // Below line let join the current path to the path of build folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  // Below code check - All of routes other than our routes, than mean (user, expense, salary etc.,).
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")); // i.e /frontend/build/index.html
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
+/* Deployment  Code End   */
+
 // Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 // Server started here
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server started on PORT ${port}`);
-});
+
+app.listen(
+  port,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${port}..`
+  )
+);
+
+// app.listen(port, () => {
+//   console.log(`Server started on PORT ${port}`);
+// });
