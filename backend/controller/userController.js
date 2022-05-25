bea2434881aeb4b300f88249b8e2a0a6a877fc0f
Vignesh R <developer.vicki@gmail.com>
2022-05-25 20:17:41 +0530

    fix: modified database connection

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -142,9 +142,8 @@ const userEmailConfirmation = asyncHandler(async (req, res) => {
 // Check User Login Status
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({
-    email: email.toLowerCase(),
-  });
+
+  const user = await User.find({ email: email.toLowerCase() });
 
   if (user && (await user.matchPassword(password))) {
     if (!user.isEmailVerified && user.emailToken !== null && !user.isActive) {
